3d8cec10d78cf6238350574714b6e81fd24d83ad
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2024-05-02 14:31:50 -0500

    added markers

--- app/src/pages/map.tsx
+++ app/src/pages/map.tsx
@@ -1,5 +1,15 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+
+const greenIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
 
 const BaseMap = () => {
   const nodes = [
@@ -13,7 +23,7 @@ const BaseMap = () => {
       <MapContainer center={[41.8781, -87.6298]} zoom={13}>
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
         {nodes.map((node) => (
-          <Marker key={node.id} position={[node.position[0], node.position[1]]}>
+          <Marker key={node.id} position={[node.position[0], node.position[1]]} icon={greenIcon}>
             <Popup>{node.name}</Popup>
           </Marker>
         ))}

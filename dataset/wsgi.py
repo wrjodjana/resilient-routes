0327a5b29651ddb38c1073191106003edfe24655
Ubuntu <ubuntu@ip-172-31-16-230.us-east-2.compute.internal>
2024-10-26 20:07:19 +0000

    added https to wsgi file

--- dataset/wsgi.py
+++ dataset/wsgi.py
@@ -1,4 +1,9 @@
-from app import app
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from dataset.app import app
 
 if __name__ == "__main__":
     app.run(debug=True)
+
